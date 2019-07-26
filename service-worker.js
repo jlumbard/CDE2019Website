fc1982de6fcf931d9f519fdbbe50536a877efce8
jlumbard <jlumbard@uwo.ca>
2019-07-25 23:33:32 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jlumbard/CDE2019Website/precache-manifest.eb8d99d4533f2e09f30df1cf83f11cc1.js"
+  "/jlumbard/CDE2019Website/src/precache-manifest.067ffadd3ca9e1baea889515d1d8b9b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jlumbard/CDE2019Website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jlumbard/CDE2019Website/src/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

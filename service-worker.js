afa0e153ce5e87086bbd77e7627f4c16b561e72f
jlumbard <jlumbard@uwo.ca>
2019-07-26 21:09:17 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CDE2019Website/precache-manifest.a195d448e3dfa0f6e83cc5bb0303f459.js"
+  "/CDE2019Website/precache-manifest.de59ee00f1a47a339c1cb91430d4954b.js"
 );
 
 self.addEventListener('message', (event) => {
